6ff458f58c8dae50b3a0d4961c6264c002f15620
Andres Perez <andy327@gmail.com>
2026-05-13 19:12:38 -0400

    add AvroSerdes with Confluent-compatible Serde for ActivityEvent

---
+++ common/src/main/scala/com/andy327/kl/common/serde/AvroSerdes.scala
@@ -0,0 +1,52 @@
+package com.andy327.kl.common.serde
+
+import java.util.{Map => JMap}
+
+import scala.jdk.CollectionConverters._
+
+import org.apache.kafka.common.serialization.{Deserializer, Serde, Serdes, Serializer}
+
+import io.confluent.kafka.serializers.{AbstractKafkaSchemaSerDeConfig, KafkaAvroDeserializer, KafkaAvroSerializer}
+
+import com.sksamuel.avro4s.RecordFormat
+
+import org.apache.avro.generic.GenericRecord
+
+import com.andy327.kl.common.model.ActivityEvent
+
+/** Avro-based `Serde` instances for pipeline domain types, backed by Confluent Schema Registry. */
+object AvroSerdes {
+
+  /** Returns a `Serde[ActivityEvent]` configured to use Confluent Schema Registry for Avro serialization.
+    *
+    * @param schemaRegistryUrl URL of the Confluent Schema Registry
+    */
+  def activityEvent(schemaRegistryUrl: String): Serde[ActivityEvent] = {
+    val configs = Map(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG -> schemaRegistryUrl).asJava
+    val ser = new ActivityEventSerializer
+    val de = new ActivityEventDeserializer
+    ser.configure(configs, false)
+    de.configure(configs, false)
+    Serdes.serdeFrom(ser, de)
+  }
+
+  private class ActivityEventSerializer extends Serializer[ActivityEvent] {
+    private val inner = new KafkaAvroSerializer()
+    private val format = RecordFormat[ActivityEvent]
+
+    override def configure(configs: JMap[String, _], isKey: Boolean): Unit = inner.configure(configs, isKey)
+    override def serialize(topic: String, data: ActivityEvent): Array[Byte] =
+      if (data == null) null else inner.serialize(topic, format.to(data))
+    override def close(): Unit = inner.close()
+  }
+
+  private class ActivityEventDeserializer extends Deserializer[ActivityEvent] {
+    private val inner = new KafkaAvroDeserializer()
+    private val format = RecordFormat[ActivityEvent]
+
+    override def configure(configs: JMap[String, _], isKey: Boolean): Unit = inner.configure(configs, isKey)
+    override def deserialize(topic: String, data: Array[Byte]): ActivityEvent =
+      if (data == null) null else format.from(inner.deserialize(topic, data).asInstanceOf[GenericRecord])
+    override def close(): Unit = inner.close()
+  }
+}
